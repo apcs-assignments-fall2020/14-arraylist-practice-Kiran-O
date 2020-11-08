4c38b329154f1420f29e4462e419d46f3d6f6a61
Kiran-O <22kokelly@athenian.org>
2020-11-08 00:49:27 +0000

    Passed All Tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,27 +1,53 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int c = 0;
+        for (int i = 0; i < list.size(); i++){
+            int k = list.get(i);
+            if (k % 2 != 0){
+                c++;
+            }
+        }
+        return c;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
+        for (int i = 0; i < list1.size(); i++){
+            int newlst1 = list1.get(i);
+            for (int k = 0; k < list2.size(); k++){
+                int newlst2 = list2.get(k);
+                if (newlst1 == newlst2){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> arr_list = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++){
+            arr_list.add(i, arr[i]);
+        }
+        return arr_list;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers do you want to enter?");
+        int num = scan.nextInt();
+        int array[] = new int[num];
+        System.out.println("Enter the " + num + " numbers now.");
+        for (int i = 0 ; i < array.length; i++ ) {
+           array[i] = scan.nextInt();
+        convertToArrayList(array);
     }
+}
 }
